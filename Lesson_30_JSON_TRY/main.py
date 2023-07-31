d8074356897b1d664905bf617f401b1a42bd0575
bebopjunky <richardjohnsmith@gmail.com>
2023-07-31 12:22:17 +0100

    Completed

--- Lesson_30_JSON_TRY/main.py
+++ Lesson_30_JSON_TRY/main.py
@@ -60,7 +60,25 @@ def save():
             website_entry.delete(0, 'end')
             password_entry.delete(0, 'end')
 
-    # delete function
+
+# ---------------------------- SEARCH PASSWORD ------------------------------- #
+def search():
+    website = website_entry.get()
+    if len(website) == 0:
+        messagebox.showerror(title="Empty Fields", message="Do not leave empty fields")
+    else:
+        try:
+            with open("data.json", "r") as f:
+                data = json.load(f)
+        except FileNotFoundError:
+            messagebox.showerror(title="EmptyDB", message="The password DB is Empty")
+        else:
+            if website in data:
+                username = data[website]["username"]
+                password = data[website]["password"]
+                messagebox.showinfo(title="Password", message=f"username: {username}\n,password: {password}")
+            else:
+                messagebox.showerror(title="No Website", message="Website not found")
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -78,9 +96,9 @@ username_label = Label(text="Username")
 password_label = Label(text="Password")
 
 website_entry = Entry()
-website_entry.config(width=52)
+website_entry.config(width=33)
 website_entry.focus()
-website_entry.grid(column=1, row=1, columnspan=2)
+website_entry.grid(column=1, row=1)
 
 username_entry = Entry()
 username_entry.config(width=52)
@@ -91,6 +109,9 @@ password_entry = Entry()
 password_entry.config(width=33)
 password_entry.grid(column=1, row=3)
 
+search_button = Button(text="Search", command=search, width=14)
+search_button.grid(column=2, row=1
+                   )
 generate_button = Button(text="Generate Password", command=generate)
 generate_button.grid(column=2, row=3)
 
